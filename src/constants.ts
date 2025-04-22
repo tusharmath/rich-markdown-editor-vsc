5e0a5a819adc9a39dd1819af71f30d3decb84a20
Tushar Mathur <tusharmath@gmail.com>
2025-04-22 18:43:57 +0530

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_FONT_SIZE = "16px"
-export const DEFAULT_FONT_FAMILY = "var(--rich-markdown-editor-font-family)"
+export const DEFAULT_FONT_SIZE = "var(--markdown-font-size)"
+export const DEFAULT_FONT_FAMILY = "var(--markdown-font-family)"
